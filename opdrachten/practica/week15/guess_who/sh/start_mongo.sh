79801fedb226f683f00e4c02da6de2c21344bec8
MarcRotsaert <10650705@hr.nl>
2022-12-16 16:18:21 +0100

    testen webscraping met selenium en bs

--- opdrachten/practica/week15/guess_who/sh/start_mongo.sh
+++ opdrachten/practica/week15/guess_who/sh/start_mongo.sh
@@ -21,10 +21,10 @@ esac
 
 containerName="python-ai-mongo"
 
-composePath="docker/compose"
-composefile="${composePath}/python-ai-mongo.yaml"
+composePath="../docker/compose/"
+composefile="${composePath}python-ai-mongo.yaml"
 
-cmd="${prefix}docker/compose/up.sh ${composefile}"
+cmd="${prefix}../docker/compose/up.sh ${composefile}"
 
 printf "%s cmd : \n\t%s\n\n" "$0" "${cmd}"
 eval ${cmd}
--- projects/seleniumtest.py
+++ projects/seleniumtest.py
@@ -1,8 +1,30 @@
 #  webscraping met selenium
-import matplotlib
-import selenium
+# import matplotlib
+# import tensorflow
+# import selenium
+import pprint
 from selenium import webdriver as wd
+import bs4
+
+# from selenium import webdriver as wd
+# from selenium import webdriver as wd
+from selenium.webdriver.common.by import By
 
 wd_inst = wd.Edge()
-wd_inst.get("http://www.knmi.nl")
-print(wd_inst.title)
+
+wd_inst.get("https://medisch-centrum-randstad.netlify.app")
+dump = wd_inst.page_source  #
+# xx
+# print(wd_inst.title)
+# print(dir(wd_inst))
+x2 = bs4.BeautifulSoup(dump)
+x3 = x2.find_all("tr")
+x4 = x3[0].find_all("td")
+
+x = wd_inst.find_element(By.CLASS_NAME, "td")
+# x = wd_inst.find_element(By.TAG_NAME, "<td>")
+
+print(x)
+print(len(x.value_of_css_property("h1")))
+# dir(By)
+# wd_inst.find_element_by_xpath
